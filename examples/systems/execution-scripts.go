6ac3d8fc11c77082b8e7dddca0a7b157051e7e0a
Georgi Kanchev <gogata.199999@gmail.com>
2025-10-06 21:18:34 +0300

    Example execution scripts

--- examples/systems/execution-scripts.go
+++ examples/systems/execution-scripts.go
@@ -2,14 +2,35 @@ package example
 
 import (
 	"fmt"
-	"pure-kit/engine/data/file"
 	"pure-kit/engine/execution/script"
+	"pure-kit/engine/utility/text"
 )
 
 func Scripts() {
 	var scr = script.New()
-	var file = file.LoadText("examples/data/script.lua")
-	scr.ExecuteCode(file)
-	var result = scr.ExecuteFunction("Greet", "tosho")
-	fmt.Printf("result: %v\n", result)
+
+	scr.AddFunction("ToLower", func(value string) string {
+		return text.LowerCase(value)
+	})
+	scr.ExecuteCode(`
+function Greet(name)
+    print("Hello, " .. name .. "!")
+end
+
+function Add(a, b)
+    return a + b
+end
+
+function ToLowerAndHi(value)
+	return ToLower(value) .. " and Hi!"
+end
+
+function IsEven(num)
+    return num % 2 == 0
+end`)
+	scr.ExecuteFunction("Greet", "tosho")
+	fmt.Printf("Add 3 + 4: %v\n", scr.ExecuteFunction("Add", 3, 4))
+	fmt.Printf("2 is even: %v\n", scr.ExecuteFunction("IsEven", 2))
+	fmt.Printf("lowercase: %v\n", scr.ExecuteFunction("ToLower", "Hello, World!"))
+	fmt.Printf("ToLowerAndHi: %v\n", scr.ExecuteFunction("ToLowerAndHi", "TESTING"))
 }
