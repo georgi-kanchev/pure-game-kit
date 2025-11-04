55f28ec424a3df3e893206be39233c8ad041f34e
Georgi Kanchev <gogata.199999@gmail.com>
2025-11-05 01:24:35 +0200

    Graphics draw polygon

--- graphics/camera-draw-primitives.go
+++ graphics/camera-draw-primitives.go
@@ -192,6 +192,25 @@ func (camera *Camera) DrawRectangle(x, y, width, height, angle float32, colors .
 	camera.end()
 	camera.Angle = prevAng
 }
+func (camera *Camera) DrawShape(color uint, points ...[2]float32) {
+	camera.begin()
+	defer camera.end()
+	if len(points) < 3 {
+		return
+	}
+
+	var triangles = triangulate(points)
+	if len(triangles) == 0 {
+		return
+	}
+
+	for _, tri := range triangles {
+		var v1 = rl.NewVector2(tri[0][0], tri[0][1])
+		var v2 = rl.NewVector2(tri[1][0], tri[1][1])
+		var v3 = rl.NewVector2(tri[2][0], tri[2][1])
+		rl.DrawTriangle(v3, v2, v1, rl.GetColor(color))
+	}
+}
 
 func (camera *Camera) DrawTexture(textureId string, x, y, width, height, angle float32, color uint) {
 	if !camera.isAreaVisible(x, y, width, height, angle) {
@@ -246,4 +265,101 @@ func (camera *Camera) DrawText(fontId, text string, x, y, height float32, color 
 	}
 
 	camera.end()
+}
+
+//=================================================================
+// private
+
+func triangulate(points [][2]float32) [][3][2]float32 {
+	var n = len(points)
+	if n < 3 {
+		return nil
+	}
+
+	var triangles [][3][2]float32
+	var verts = make([]int, n)
+	for i := range n {
+		verts[i] = i
+	}
+
+	var ccw = area(points) > 0
+	for len(verts) > 3 {
+		var earFound = false
+
+		for i := 0; i < len(verts); i++ {
+			var prev = verts[(i+len(verts)-1)%len(verts)]
+			var curr = verts[i]
+			var next = verts[(i+1)%len(verts)]
+			var p1 = points[prev]
+			var p2 = points[curr]
+			var p3 = points[next]
+
+			if !isEar(points, verts, prev, curr, next, ccw) {
+				continue
+			}
+
+			triangles = append(triangles, [3][2]float32{p1, p2, p3})
+			verts = append(verts[:i], verts[i+1:]...)
+			earFound = true
+			break
+		}
+
+		if !earFound {
+			break // If no ear found, polygon might be degenerate or self-intersecting
+		}
+	}
+
+	if len(verts) == 3 {
+		triangles = append(triangles, [3][2]float32{
+			points[verts[0]],
+			points[verts[1]],
+			points[verts[2]],
+		})
+	}
+
+	return triangles
+}
+func area(points [][2]float32) float32 {
+	var a float32
+	for i := range points {
+		var j = (i + 1) % len(points)
+		a += points[i][0]*points[j][1] - points[j][0]*points[i][1]
+	}
+	return a / 2
+}
+func isEar(points [][2]float32, verts []int, i1, i2, i3 int, ccw bool) bool {
+	var p1, p2, p3 = points[i1], points[i2], points[i3]
+	var cross = (p2[0]-p1[0])*(p3[1]-p1[1]) - (p2[1]-p1[1])*(p3[0]-p1[0])
+	if ccw && cross <= 0 {
+		return false
+	}
+	if !ccw && cross >= 0 {
+		return false
+	}
+
+	for _, vi := range verts {
+		if vi == i1 || vi == i2 || vi == i3 {
+			continue
+		}
+		if pointInTriangle(points[vi], p1, p2, p3) {
+			return false
+		}
+	}
+
+	return true
+}
+func pointInTriangle(p, a, b, c [2]float32) bool {
+	var v0 = [2]float32{c[0] - a[0], c[1] - a[1]}
+	var v1 = [2]float32{b[0] - a[0], b[1] - a[1]}
+	var v2 = [2]float32{p[0] - a[0], p[1] - a[1]}
+	var dot00 = v0[0]*v0[0] + v0[1]*v0[1]
+	var dot01 = v0[0]*v1[0] + v0[1]*v1[1]
+	var dot02 = v0[0]*v2[0] + v0[1]*v2[1]
+	var dot11 = v1[0]*v1[0] + v1[1]*v1[1]
+	var dot12 = v1[0]*v2[0] + v1[1]*v2[1]
+	var invDenom = 1 / (dot00*dot11 - dot01*dot01)
+	var u = (dot11*dot02 - dot01*dot12) * invDenom
+	var v = (dot00*dot12 - dot01*dot02) * invDenom
+
+	return (u >= 0) && (v >= 0) && (u+v <= 1)
 }
