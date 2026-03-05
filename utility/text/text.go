af64a8bf305f9779306089cfb0b3f0fce37d38e1
Georgi Kanchev <gogata.199999@gmail.com>
2026-03-05 22:27:16 +0200

    Package text builder file

--- utility/text/text.go
+++ utility/text/text.go
@@ -10,16 +10,7 @@ import (
 	"reflect"
 
 	"strconv"
-	"strings"
 )
-
-type Builder struct{ buffer *strings.Builder }
-
-func NewBuilder() *Builder                       { return &Builder{buffer: &strings.Builder{}} }
-func (builder *Builder) WriteText(text string)   { builder.buffer.WriteString(text) }
-func (builder *Builder) WriteSymbol(symbol rune) { builder.buffer.WriteRune(symbol) }
-func (builder *Builder) Clear()                  { builder.buffer.Reset() }
-func (builder *Builder) ToText() string          { return builder.buffer.String() }
 
 func New(elements ...any) string {
 	var builder = NewBuilder()
