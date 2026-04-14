c72b9786724ea6d0813150bccc591a22b02ba4b7
Georgi Kanchev <gogata.199999@gmail.com>
2026-04-14 03:24:14 +0300

    Area constructor

--- graphics/graphics.go
+++ graphics/graphics.go
@@ -21,6 +21,10 @@ import (
 
 type Area struct{ X, Y, Width, Height float32 }
 
+func NewArea(x, y, width, height float32) Area {
+	return Area{X: x, Y: y, Width: width, Height: height}
+}
+
 //=================================================================
 // private
 
