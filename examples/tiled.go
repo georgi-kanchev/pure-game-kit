7595fd269a5fbde87d609f154cfb77540a2ad6b0
Georgi Kanchev <gogata.199999@gmail.com>
2026-07-08 16:06:53 +0300

    Performance tests

--- examples/tiled.go
+++ examples/tiled.go
@@ -1,12 +1,10 @@
 package example
 
 import (
-	"fmt"
 	"pure-game-kit/packages/assets"
 	"pure-game-kit/packages/geometry"
 	"pure-game-kit/packages/graphics"
 	"pure-game-kit/packages/utility/color/palette"
-	"pure-game-kit/packages/utility/debug"
 	"pure-game-kit/packages/window"
 )
 
@@ -25,8 +23,6 @@ func Tiled() {
 	var cellShapes = layers[3].TilemapShapes()
 
 	layers[0].TileLayerId.SetTile(0, 0, assets.NewTile(55))
-
-	fmt.Printf("debug.LinesOfCode(): %v\n", debug.LinesOfCode())
 
 	window.SetTargetFPS(0)
 
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	example.GUI()
+	example.Tiled()
 }
